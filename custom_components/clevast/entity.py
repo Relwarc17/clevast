ae027812c637d882eeb292262514a87f836fd80c
relwarc17 <28979675+Relwarc17@users.noreply.github.com>
2025-03-08 19:42:08 +0100

    asda

--- custom_components/clevast/entity.py
+++ custom_components/clevast/entity.py
@@ -1,6 +1,7 @@
 """ClevastEntity class"""
 from homeassistant.helpers.update_coordinator import CoordinatorEntity
 from homeassistant.components.humidifier import HumidifierEntity
+from homeassistant.core import callback
 
 from .const import ATTRIBUTION
 from .const import DOMAIN
@@ -8,9 +9,9 @@ from .const import NAME
 from .const import VERSION
 
 
-class ClevastEntity(CoordinatorEntity):
+class ClevastEntity(CoordinatorEntity, HumidifierEntity):
     def __init__(self, coordinator, config_entry):
-        super().__init__(coordinator)
+        super().__init__(coordinator, config_entry)
         self.config_entry = config_entry
 
     @property
@@ -35,3 +36,44 @@ class ClevastEntity(CoordinatorEntity):
             "id": str(self.coordinator.data.get("id")),
             "integration": DOMAIN,
         }
+
+    async def set_mode(self, mode):
+        """Set new target preset mode."""
+         # Do the turning off.
+        # ...
+
+        # Update the data
+        await self.coordinator.async_request_refresh()
+
+
+    async def set_humidity(self, humidity):
+        """Set new target humidity."""
+         # Do the turning off.
+        # ...
+
+        # Update the data
+        await self.coordinator.async_request_refresh()
+
+    async def turn_on(self, **kwargs):
+        """Turn the device on."""
+         # Do the turning on.
+        # ...
+
+        # Update the data
+        await self.coordinator.async_request_refresh()
+
+    async def turn_off(self, **kwargs):
+        """Turn the device off."""
+         # Do the turning off.
+        # ...
+
+        # Update the data
+        await self.coordinator.async_request_refresh()
+
+    @callback
+    def _handle_coordinator_update(self) -> None:
+        """Handle updated data from the coordinator."""
+        self._attr_is_on = self.coordinator.data[self.config_entry]["state"]
+        self.async_write_ha_state()
+
+       
